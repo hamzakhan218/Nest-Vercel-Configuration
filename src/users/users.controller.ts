0e6dcbd65fdb8ed07fb770622cbdd625f033fcff
Hamza Bashir <hamzabashir218@gmail.com>
2023-08-24 20:17:34 +0500

    users api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+     getUser(): string{
+        return "This will return all the users in the Database."
+    }
+
+    @Post(":id")
+    createUser(@Param("id") id: string, @Body() usersBody: {
+        email: string,
+        password: string
+    } ): string{
+        return `User will be created with id ${id} using these credentials email: ${usersBody.email} and password: ${usersBody.password}`
+
+    }
+}
